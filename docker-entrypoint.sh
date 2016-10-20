46b52ee78dbf84ccff139665c000250013c51f60
athenaspa <athenaspa@users.noreply.github.com>
2016-10-20 17:04:08 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,12 @@
 set -e
 
 # Post deployment Assets warm up
-echo "Installing Composer Dependencies"
-cd /var/www/html && composer install --no-interaction;
+cd /var/www/html
+composer install --no-interaction
 if [ ! -f /var/www/html/web/sites/default/settings.local.php ];
   then
-      echo "Setting up Drupal"
-      cd /var/www/html && vendor/bin/robo config:settings;
-  fi
+      vendor/bin/robo config:settings;
+fi
 
 # Start supervisord
 echo "Start Supervisord"
