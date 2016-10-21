62609a81907d5bc15f56c9335117e0f1ba64ce59
athenaspa <athenaspa@users.noreply.github.com>
2016-10-21 11:51:26 +0200

    Fixing Typos

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 # Post deployment Assets warm up
 cd /var/www/html
-composer install --no-interaction > /dev/null 2>&1
+composer install --no-interaction --quiet
 if [ ! -f /var/www/html/web/sites/default/settings.local.php ];
   then
-      vendor/bin/robo config:settings > /dev/null 2>&1;
+      vendor/bin/robo config:settings >/dev/null 2>&1
 fi
 
-# Start supervisord
+# Start Apache2 in the foreground
 /usr/local/bin/apache2-foreground
