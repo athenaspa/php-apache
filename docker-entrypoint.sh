de67931425398ec258134433979c6f6c8a4091c2
athenaspa <athenaspa@users.noreply.github.com>
2016-11-29 09:14:33 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,7 @@ if [ ! -f /var/www/html/web/sites/default/settings.local.php ];
   then
       vendor/bin/robo config:settings >/dev/null 2>&1
 fi
+vendor/bin/robo site:update >/dev/null 2>&1
 
 # Start Apache2 in the foreground
 /usr/local/bin/apache2-foreground
