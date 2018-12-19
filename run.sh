a445e7f648d4be2a8f2c72fbf1daa3e32db73aeb
ATHENA <athenaspa@users.noreply.github.com>
2018-12-19 15:33:13 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#change the UID/GID of Apache
+
+APACHE_RUN_USER=${APACHE_RUN_USER:=33}
+APACHE_RUN_GROUP=${APACHE_RUN_GROUP:=33}
+
+usermod -u $APACHE_RUN_USER www-data
+groupmod -g $APACHE_RUN_GROUP www-data
+
+chown -R www-data:www-data /var/www/html
+
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid && \
 sed -i "s|DOCUMENT_ROOT|$DOCUMENT_ROOT|g" /etc/apache2/sites-available/default && \
