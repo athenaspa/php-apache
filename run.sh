75c4b01440c66ea736eef04f88df169a3fc9500a
ATHENA <athenaspa@users.noreply.github.com>
2018-12-19 16:00:10 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ chown -R www-data:www-data /var/www/html
 
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid && \
+rm -rf /var/lock/apache2 && \
 sed -i "s|DOCUMENT_ROOT|$DOCUMENT_ROOT|g" /etc/apache2/sites-available/default && \
 sed -i "s|ENVIRONMENT_VAR|$ENVIRONMENT|g" /etc/apache2/sites-available/default && \
 /usr/sbin/apache2ctl -D FOREGROUND
